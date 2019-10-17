db35dcdcfacb2bc35b29cf3a87195691ccb1f0fb
ironest <ironest@gmail.com>
2019-10-17 14:43:37 +1100

    Added counts to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
 
     def home
+
+        @flavour_count = Ingredient.flavour.count
+        @topping_count = Ingredient.topping.count
+
     end
 
 end
